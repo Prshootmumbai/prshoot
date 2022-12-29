7370a8db9e15cb4b549b8eb6565fbb8342b34ec5
prateek.singh <prateek.singh@ultragenicglobal.com>
2022-12-29 22:01:07 +0530

    add headers

--- assets/js/API/contactApi.js
+++ assets/js/API/contactApi.js
@@ -1,67 +1,66 @@
-const url = 'https://2oj2z3lfgfo5uhn4i4v7jtgdjm0jerjr.lambda-url.us-east-1.on.aws/'
+const url =
+  'https://2oj2z3lfgfo5uhn4i4v7jtgdjm0jerjr.lambda-url.us-east-1.on.aws/';
 function validationForm() {
+  // $("#contactForm").submit(function (e) {
+  //     e.preventDefault();
+  // });
 
+  // var form = document.getElementById("contactForm");
+  // function handleForm(event) { event.preventDefault(); }
+  // form.addEventListener('submit', handleForm);
+  // e.preventDefault();
+  const name = document.getElementById('inputName').value;
+  const email = document.getElementById('inputEmail').value;
+  const mobile = document.getElementById('inputPhone').value;
+  // var faddress = document.getElementById('inputAddress').value;
+  const message = document.getElementById('inputMessage').value;
 
-    // $("#contactForm").submit(function (e) {
-    //     e.preventDefault();
-    // });
+  console.log('fname', name);
+  console.log('femail', email);
 
-    // var form = document.getElementById("contactForm");
-    // function handleForm(event) { event.preventDefault(); }
-    // form.addEventListener('submit', handleForm);
-    // e.preventDefault();
-    const name = document.getElementById('inputName').value;
-    const email = document.getElementById('inputEmail').value
-    const mobile = document.getElementById('inputPhone').value;
-    // var faddress = document.getElementById('inputAddress').value;
-    const message = document.getElementById('inputMessage').value
+  sendEmail(url, { name, email, mobile, message });
 
-    console.log("fname", name)
-    console.log("femail", email)
+  // if (userName.value.trim() === '') {
+  // 	seterror(userName, 'name length require')
+  // 	retunval = false;
+  // }
 
-    sendEmail(url, { name, email, mobile, message })
+  // 	var email = document.forms['myForm']['femail'].value;
+  // 	if (name.length < 5) {
+  // 		seterror('email', 'email length require')
+  // 		retunval = false;
+  // 	}
 
-    // if (userName.value.trim() === '') {
-    // 	seterror(userName, 'name length require')
-    // 	retunval = false;
-    // }
+  // 	var phone = document.forms['myForm']['fphone'].value;
+  // 	if (phone.length < 5) {
+  // 		seterror('phone', 'phone length require')
+  // 		retunval = false;
+  // 	}
+  // 	var address = document.forms['myForm']['faddress'].value;
+  // 	if (address.length < 5) {
+  // 		seterror('address', 'address length require')
+  // 		retunval = false;
+  // 	}
+  // 	var message = document.forms['myForm']['fmessage'].value;
+  // 	if (message.length < 5) {
+  // 		seterror('message', 'message length require')
+  // 		retunval = false;
+  // 	}
 
-    // 	var email = document.forms['myForm']['femail'].value;
-    // 	if (name.length < 5) {
-    // 		seterror('email', 'email length require')
-    // 		retunval = false;
-    // 	}
-
-    // 	var phone = document.forms['myForm']['fphone'].value;
-    // 	if (phone.length < 5) {
-    // 		seterror('phone', 'phone length require')
-    // 		retunval = false;
-    // 	}
-    // 	var address = document.forms['myForm']['faddress'].value;
-    // 	if (address.length < 5) {
-    // 		seterror('address', 'address length require')
-    // 		retunval = false;
-    // 	}
-    // 	var message = document.forms['myForm']['fmessage'].value;
-    // 	if (message.length < 5) {
-    // 		seterror('message', 'message length require')
-    // 		retunval = false;
-    // 	}
-
-
-    // 	return false;
+  // 	return false;
 }
 
-
-
-
 async function sendEmail(url, body) {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body
-    });
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
+    body,
+  });
 
-    const result = await response.json();
-    console.log(result)
+  const result = await response.json();
+  console.log(result);
 }
